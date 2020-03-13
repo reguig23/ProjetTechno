2622e2796f3210528992cf88c6f03b791c70d35a
Reguig <eruptor231998@gmail.com>
2020-03-13 18:09:06 +0100

    Ajout de Link pour pouvoir passer d'une page a une autre

--- projet/src/PageAccueil.js
+++ projet/src/PageAccueil.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './PageAccueil.css';
+import Formulaire from './PageInscription';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom' /* Moyen de changer de page react*/ 
 
+const Home = ()=>( /* Max tu ecrira la Page d'accueil ici avec une class pour ton formulaire pour les differents liens fait comme j'ai fait avec le link */
+  <div>
+    <Link className="btn" to={'/PageInscription'}>S'incrire</Link>
+    
+  </div>
+  
+)
+/*Donc pour la Balise Link voici le lien où je l'ai trouve : https://openclassrooms.com/fr/courses/4555591-creer-un-petit-jeu-de-role-avec-reactjs/4555606-concevoir-le-layout-de-notre-application?status=waiting-for-publication
+J'arrive jamais à explique les concepts desole !!! Normalement tu as besoin de rien rajouter car j'ai tous fait(j'ai installer les dependances liées au Link Route Routeur )  a part les balises bien evidemment */
 
-function PageAccueil() {
-  const lien = <a href="#" > lien </a>; 
-    return (<div><h1>bonjour</h1> {lien}</div> 
+class PageAccueil extends React.Component{
+  
+  render(){
+    return (
+      <Router>
+      <div>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/PageInscription" component={Formulaire}></Route>
+      </div>
+    </Router>
     )
-
-
   }
+}
 
   export default PageAccueil;
--- projet/src/index.js
+++ projet/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-/*import PageAccueil from './PageAccueil';*/
+import PageAccueil from './PageAccueil';
 import Formulaire from './PageInscription';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Formulaire />, document.getElementById('root'));
+ReactDOM.render(<PageAccueil/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

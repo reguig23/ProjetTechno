18c200d913f3b7641a174ced1ba2624ba00a99ef
Reguig <eruptor231998@gmail.com>
2020-03-13 18:23:35 +0100

    Guide pour Max avec commentaire Jespere que ca te sera utile

--- projet/src/PageAccueil.js
+++ projet/src/PageAccueil.js
@@ -3,8 +3,12 @@ import './PageAccueil.css';
 import Formulaire from './PageInscription';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom' /* Moyen de changer de page react*/ 
 
+class SeConnecter extends React.Component{ /* Regarde mon formulaire est fait pareil et trouve un moyen pour la fonction submit si c'est bon alors on change de page et si c'est faux alors on rajoute un texte en rouge iden ou mdp incorrect  */
+
+}
 const Home = ()=>( /* Max tu ecrira la Page d'accueil ici avec une class pour ton formulaire pour les differents liens fait comme j'ai fait avec le link */
   <div>
+    <SeConnecter/>
     <Link className="btn" to={'/PageInscription'}>S'incrire</Link>
     
   </div>
@@ -12,7 +16,7 @@ const Home = ()=>( /* Max tu ecrira la Page d'accueil ici avec une class pour to
 )
 /*Donc pour la Balise Link voici le lien où je l'ai trouve : https://openclassrooms.com/fr/courses/4555591-creer-un-petit-jeu-de-role-avec-reactjs/4555606-concevoir-le-layout-de-notre-application?status=waiting-for-publication
 J'arrive jamais à explique les concepts desole !!! Normalement tu as besoin de rien rajouter car j'ai tous fait(j'ai installer les dependances liées au Link Route Routeur )  a part les balises bien evidemment 
-Et pour repondre a de futur question (Donc React n'est pas sur une seul page?) ,la reponse est oui et non car on a besoin d'autre page car on la divise en plusieur app mais les modif ne se font que sur une page */
+Et pour repondre a de futur question (React n'est pas sur une seul page?) ,la reponse est oui et non car on a besoin d'autre page car on la divise en plusieur app mais les modif ne se font que sur une page (celle ou on est sans modif les autres ) */
 
 class PageAccueil extends React.Component{
   
